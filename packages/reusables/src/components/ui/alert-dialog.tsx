903059c91e93524dfc0992ce868cb5a4bfcec915
Zach Nugent <mrzachnugent@gmail.com>
2025-01-18 08:39:27 -0500

    feat(docs): add alert-dialog

--- packages/reusables/src/components/ui/alert-dialog.tsx
+++ packages/reusables/src/components/ui/alert-dialog.tsx
@@ -20,7 +20,7 @@ const AlertDialogOverlayWeb = React.forwardRef<
   return (
     <AlertDialogPrimitive.Overlay
       className={cn(
-        'z-50 bg-black/80 flex justify-center items-center p-2 absolute top-0 right-0 bottom-0 left-0',
+        'z-50 bg-black/80 flex justify-center items-center p-2 fixed top-0 right-0 bottom-0 left-0',
         open ? 'web:animate-in web:fade-in-0' : 'web:animate-out web:fade-out-0',
         className
       )}
