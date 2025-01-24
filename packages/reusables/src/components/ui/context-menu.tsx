13f4e607431afb0d95170506f4f31e8659c845bc
Zach Nugent <mrzachnugent@gmail.com>
2025-01-24 15:18:46 -0500

    fix(reusables): context-menu checkmark and radio items text

--- packages/reusables/src/components/ui/context-menu.tsx
+++ packages/reusables/src/components/ui/context-menu.tsx
@@ -142,22 +142,24 @@ const ContextMenuCheckboxItem = React.forwardRef<
   ContextMenuPrimitive.CheckboxItemRef,
   ContextMenuPrimitive.CheckboxItemProps
 >(({ className, children, ...props }, ref) => (
-  <ContextMenuPrimitive.CheckboxItem
-    ref={ref}
-    className={cn(
-      'relative flex flex-row web:cursor-default items-center web:group rounded-sm py-1.5 native:py-2 pl-8 pr-2 web:outline-none web:focus:bg-accent active:bg-accent',
-      props.disabled && 'web:pointer-events-none opacity-50',
-      className
-    )}
-    {...props}
-  >
-    <View className='absolute left-2 flex h-3.5 w-3.5 items-center justify-center'>
-      <ContextMenuPrimitive.ItemIndicator>
-        <Check size={14} strokeWidth={3} className='text-foreground' />
-      </ContextMenuPrimitive.ItemIndicator>
-    </View>
-    <>{children}</>
-  </ContextMenuPrimitive.CheckboxItem>
+  <TextClassContext.Provider value='select-none text-sm native:text-lg text-popover-foreground web:group-focus:text-accent-foreground'>
+    <ContextMenuPrimitive.CheckboxItem
+      ref={ref}
+      className={cn(
+        'relative flex flex-row web:cursor-default items-center web:group rounded-sm py-1.5 native:py-2 pl-8 pr-2 web:outline-none web:focus:bg-accent active:bg-accent',
+        props.disabled && 'web:pointer-events-none opacity-50',
+        className
+      )}
+      {...props}
+    >
+      <View className='absolute left-2 flex h-3.5 w-3.5 items-center justify-center'>
+        <ContextMenuPrimitive.ItemIndicator>
+          <Check size={14} strokeWidth={3} className='text-foreground' />
+        </ContextMenuPrimitive.ItemIndicator>
+      </View>
+      <>{children}</>
+    </ContextMenuPrimitive.CheckboxItem>
+  </TextClassContext.Provider>
 ));
 ContextMenuCheckboxItem.displayName = ContextMenuPrimitive.CheckboxItem.displayName;
 
@@ -165,22 +167,24 @@ const ContextMenuRadioItem = React.forwardRef<
   ContextMenuPrimitive.RadioItemRef,
   ContextMenuPrimitive.RadioItemProps
 >(({ className, children, ...props }, ref) => (
-  <ContextMenuPrimitive.RadioItem
-    ref={ref}
-    className={cn(
-      'relative flex flex-row web:cursor-default web:group items-center rounded-sm py-1.5 native:py-2 pl-8 pr-2 web:outline-none web:focus:bg-accent active:bg-accent',
-      props.disabled && 'web:pointer-events-none opacity-50',
-      className
-    )}
-    {...props}
-  >
-    <View className='absolute left-2 flex h-3.5 w-3.5 items-center justify-center'>
-      <ContextMenuPrimitive.ItemIndicator>
-        <View className='bg-foreground h-2 w-2 rounded-full' />
-      </ContextMenuPrimitive.ItemIndicator>
-    </View>
-    <>{children}</>
-  </ContextMenuPrimitive.RadioItem>
+  <TextClassContext.Provider value='select-none text-sm native:text-lg text-popover-foreground web:group-focus:text-accent-foreground'>
+    <ContextMenuPrimitive.RadioItem
+      ref={ref}
+      className={cn(
+        'relative flex flex-row web:cursor-default web:group items-center rounded-sm py-1.5 native:py-2 pl-8 pr-2 web:outline-none web:focus:bg-accent active:bg-accent',
+        props.disabled && 'web:pointer-events-none opacity-50',
+        className
+      )}
+      {...props}
+    >
+      <View className='absolute left-2 flex h-3.5 w-3.5 items-center justify-center'>
+        <ContextMenuPrimitive.ItemIndicator>
+          <View className='bg-foreground h-2 w-2 rounded-full' />
+        </ContextMenuPrimitive.ItemIndicator>
+      </View>
+      <>{children}</>
+    </ContextMenuPrimitive.RadioItem>
+  </TextClassContext.Provider>
 ));
 ContextMenuRadioItem.displayName = ContextMenuPrimitive.RadioItem.displayName;
 
