f8929123ee39773d9f7662eb578149b7394647b2
Zach Nugent <mrzachnugent@gmail.com>
2025-01-25 14:21:11 -0500

    feat(docs): add dialog

--- packages/reusables/src/components/ui/dialog.tsx
+++ packages/reusables/src/components/ui/dialog.tsx
@@ -19,7 +19,7 @@ const DialogOverlayWeb = React.forwardRef<DialogPrimitive.OverlayRef, DialogPrim
     return (
       <DialogPrimitive.Overlay
         className={cn(
-          'bg-black/80 flex justify-center items-center p-2 absolute top-0 right-0 bottom-0 left-0',
+          'z-50 bg-black/80 flex justify-center items-center p-2 fixed top-0 right-0 bottom-0 left-0',
           open ? 'web:animate-in web:fade-in-0' : 'web:animate-out web:fade-out-0',
           className
         )}
